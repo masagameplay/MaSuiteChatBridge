cd945027333d34a13d940dff62d4ab3ea09d5730
Masa <masa@simasasolutions.com>
2018-12-12 19:20:44 +0200

    Added nick command

---
+++ src/main/java/fi/matiaspaavilainen/masuitechat/commands/Nick.java
@@ -0,0 +1,51 @@
+package fi.matiaspaavilainen.masuitechat.commands;
+
+import fi.matiaspaavilainen.masuitechat.MaSuiteChatBridge;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Nick implements CommandExecutor {
+
+    private MaSuiteChatBridge plugin;
+
+    public Nick(MaSuiteChatBridge p) {
+        plugin = p;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        Player p = (Player) sender;
+        if (args.length == 1) {
+            try (ByteArrayOutputStream b = new ByteArrayOutputStream();
+                 DataOutputStream out = new DataOutputStream(b)) {
+                out.writeUTF("MaSuiteChat");
+                out.writeUTF("Nick");
+                out.writeUTF(args[0]);
+                p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (args.length == 2) {
+            try (ByteArrayOutputStream b = new ByteArrayOutputStream();
+                 DataOutputStream out = new DataOutputStream(b)) {
+                out.writeUTF("MaSuiteChat");
+                out.writeUTF("NickOther");
+                out.writeUTF(args[1]);
+                out.writeUTF(args[0]);
+                p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
